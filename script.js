9cd92e15a651d7bc2d693d72560228e98ca0dede
sannidhiyk <sannidhi702@gmail.com>
2024-07-12 23:27:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar").style.width = "50%";
+}
+function closeNavbar() {
+    document.getElementById("sideNavigationBar").style.width = "0%";
+}
